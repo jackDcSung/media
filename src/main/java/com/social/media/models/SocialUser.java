1a4e625a8c910afb9bb766628fadcb9bcd80006c
ASUS VivoBook <likerockman102@hotmail.com>
2024-12-06 12:31:02 +0800

    Many to Many Relationships

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -1,10 +1,11 @@
 package com.social.media.models;
 
 import jakarta.persistence.*;
-import org.springframework.context.annotation.Profile;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class SocialUser {
@@ -27,6 +28,14 @@ public class SocialUser {
     private List<Post> posts=new ArrayList<>();
 
 
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_group",
+            joinColumns =@JoinColumn(name="user_id"),
+            inverseJoinColumns  =@JoinColumn(name="group_id")
+    )
+    private Set<SocialGroup> groups=new HashSet<>();
 
 
 
